59547db9e99be6d863249ce1ee1c3311432f5e87
chalee <695469701@qq.com>
2021-07-08 14:32:09 +0800

    log

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { CDN } = process.env;
 var qiniuPrefix = require('./qiniu-upload-prefix');
 module.exports = {
-//   publicPath: CDN +  qiniuPrefix.prefix, // 对应webpack 中的 publicPath --> CDN 地址
+  publicPath: CDN +  qiniuPrefix.prefix, // 对应webpack 中的 publicPath --> CDN 地址
 };
